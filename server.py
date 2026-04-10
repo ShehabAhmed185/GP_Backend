d11a7de0e6496be627d629100d6b75f9d872121b
Shehab <shehab1852002@gmail.com>
2026-04-10 21:41:26 +0200

    working in hosting on railway

--- server.py
+++ server.py
@@ -74,5 +74,8 @@ def recommend_fertilizer():
 
 if __name__ == '__main__':
     # Run on port 5000
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
     # app.run(host='0.0.0.0', port=5000)
+    # Railway يحدد المنفذ تلقائياً عبر متغير البيئة PORT
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
